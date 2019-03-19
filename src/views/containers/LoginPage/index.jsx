597bfbae278a2e3180a955bb4a070d35bf6b6ddb
Cauê Sampaio <caue.sampaio@it4solution.com.br>
2019-03-19 16:34:11 -0300

    Adding logo, align containers

--- src/views/containers/LoginPage/index.jsx
+++ src/views/containers/LoginPage/index.jsx
@@ -1,8 +1,9 @@
-/*eslint-disable*/
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { Layout, Row, Col, Form, Input, Button } from 'antd';
+
+import logo from '../../../assets/img/white-logo.png';
 
 import './style.less';
 
@@ -23,63 +24,65 @@ class LoginPage extends Component {
     } = this.props;
     const { Content } = Layout;
     return (
-      <Layout style={{margin: "0px", background: "#fff"}}>
-        <Content style={{height:"100vh"}}>
-          <Row type="flex" justify="center" align="middle">
-            <Col type="flex" justify="center" align="middle" className="logo-container" span={14}>
-              <img src="src/assets/img/white-logo.png"></img>
-            </Col>
-            <Col type="flex" justify="center" className="login-container" span={10}>
-              <Form onSubmit={this.handleSubmit} className="login-form">
-                <Form.Item className="login-form-field" label="Email">
-                  {getFieldDecorator('userEmail', {
-                    rules: [
-                      {
-                        type: 'email',
-                        message: 'Por favor insira um email válido.',
-                      },
-                      {
-                        required: true,
-                        message: 'Por favor insira seu email.',
-                      }
-                    ],
-                  })(<Input placeholder="Email" />)}
-                </Form.Item>
-                <Form.Item className="login-form-field" label="Senha">
-                  {getFieldDecorator('password', {
-                    rules: [
-                      {
-                        required: true,
-                        message: 'Por favor insira sua senha!',
-                      },
-                    ],
-                  })(<Input type="password" placeholder="Senha" />)}
-                </Form.Item>
-                <Form.Item>
-                  <a className="login-form-forgot" href="forgot.html">
-                    Esqueceu sua senha?
-                  </a>
-                </Form.Item>
-                <Form.Item>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    className="login-form-button"
-                  >
-                    LOGIN
-                  </Button>
-                </Form.Item>
-                <Form.Item>
-                  <p>
-                    Ainda não possui uma conta?
-                    <a href="new-user.html">Cadastre-se</a>
-                  </p>
-                </Form.Item>
-              </Form>
-            </Col>
-          </Row>
-        </Content>
-      </Layout>
+      <div className="login-page">
+        <Layout style={{ background: '#fff' }}>
+          <Content style={{ height: '100vh' }}>
+            <Row type="flex" justify="space-between">
+              <Col className="logo-container" span={12}>
+                <img className="login-logo" src={logo} alt="logo" />
+              </Col>
+              <Col className="login-container" span={12}>
+                <Form onSubmit={this.handleSubmit} className="login-form">
+                  <Form.Item className="login-form-field" label="Email">
+                    {getFieldDecorator('userEmail', {
+                      rules: [
+                        {
+                          type: 'email',
+                          message: 'Por favor insira um email válido.',
+                        },
+                        {
+                          required: true,
+                          message: 'Por favor insira seu email.',
+                        },
+                      ],
+                    })(<Input placeholder="Email" />)}
+                  </Form.Item>
+                  <Form.Item className="login-form-field" label="Senha">
+                    {getFieldDecorator('password', {
+                      rules: [
+                        {
+                          required: true,
+                          message: 'Por favor insira sua senha!',
+                        },
+                      ],
+                    })(<Input type="password" placeholder="Senha" />)}
+                  </Form.Item>
+                  <Form.Item type="flex" justify="right">
+                    <a className="login-form-forgot" href="forgot.html">
+                      Esqueceu sua senha?
+                    </a>
+                  </Form.Item>
+                  <Form.Item>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      className="login-form-button"
+                    >
+                      LOGIN
+                    </Button>
+                  </Form.Item>
+                  <Form.Item>
+                    <p>
+                      Ainda não possui uma conta?
+                      <a href="new-user.html">Cadastre-se</a>
+                    </p>
+                  </Form.Item>
+                </Form>
+              </Col>
+            </Row>
+          </Content>
+        </Layout>
+      </div>
     );
   }
 }
