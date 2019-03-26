f8086afef46f5604effc99a9dfee2d177760e384
Cauê Sampaio <caue.sampaio@it4solution.com.br>
2019-03-26 16:00:46 -0300

    Listing products done, adding buttons to header, adding order number and status to header

--- src/views/containers/OrdersPage/containers/OrderDetailsPage/components/ProductsList/index.jsx
+++ src/views/containers/OrdersPage/containers/OrderDetailsPage/components/ProductsList/index.jsx
@@ -9,57 +9,53 @@ import PrivatePageSection from '../../../../../../components/PrivatePageSection'
 import './style.less';
 
 class ProductList extends Component {
-  state = {
-    products: null,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: null,
+    };
+  }
 
   renderTitle = (item) => (
-    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      <span>{item.title}</span>
-      <span>Vestuário</span>
+    <div style={{ display: 'flex', justifyContent: 'space-between',  whiteSpace: 'normal' }}>
+      {item.channelSku.description}
     </div>
   );
 
   renderAvatar = (item) => (
     <div style={{ display: 'flex' }}>
       <Checkbox />
-      <img alt="" src={item.avatar} style={{ marginLeft: 8 }} />
+      <img alt="" src={item.channelSku.images[0]} style={{ marginLeft: 8 }} />
+    </div>
+  );
+
+  renderNetValue = (item) => (
+    <div style={{ display: 'flex' }}>
+      <span>{item.netValue}}</span>
+    </div>
+  );
+
+  renderDescription = (item) => (
+    <div className="product-description">
+      <h3>Quantidade: <span>{item.quantity}</span></h3>
+      <h3>Preço: <span>{item.netValue}</span></h3>
+      <h3>SKU: <span>{item.channelSku.refSku}</span></h3>
+      <h3>Status: <span>{item.status}</span></h3>
     </div>
   );
 
   render() {
-    const list = [
-      {
-        id: 1235,
-        description: 'teste',
-        avatar:
-          'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        title: 'Tênis nike',
-      },
-      {
-        id: 78787,
-        description: 'fdhjgdhk',
-        avatar:
-          'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        title: 'Tênis asics',
-      },
-      {
-        id: 4564,
-        description: 'fdhjgdhk',
-        avatar:
-          'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        title: 'Tênis flex',
-      },
-    ];
     const { products } = this.props;
-    console.log("render", products);
+    console.log('render', products);
     return (
       <div>
         <PrivatePageSection>
           <h3>Produtos</h3>
           <Row type="flex">
             <PrivatePageHeaderButton>Faturar SKUS</PrivatePageHeaderButton>
-            <PrivatePageHeaderButton>Enviar tracking dos SKUS</PrivatePageHeaderButton>
+            <PrivatePageHeaderButton>
+              Enviar tracking dos SKUS
+            </PrivatePageHeaderButton>
             <PrivatePageHeaderButton>Cancelar produto</PrivatePageHeaderButton>
           </Row>
           <Row>
@@ -69,16 +65,15 @@ class ProductList extends Component {
           </Row>
           <List
             rowKey="id"
-            // loading={loading}
             grid={{ gutter: 24, lg: 3, md: 2, sm: 1, xs: 1 }}
-            dataSource={[...list]}
+            dataSource={[...products]}
             renderItem={(item) => (
               <List.Item key={item.id}>
                 <Card hoverable className="card">
                   <Card.Meta
                     avatar={this.renderAvatar(item)}
                     title={this.renderTitle(item)}
-                    description={item.description}
+                    description={this.renderDescription(item)}
                   />
                 </Card>
               </List.Item>
--- src/views/containers/OrdersPage/containers/OrderDetailsPage/index.jsx
+++ src/views/containers/OrdersPage/containers/OrderDetailsPage/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose, bindActionCreators } from 'redux';
 import { createStructuredSelector } from 'reselect';
-import { Card, Row, Col, Button, Icon } from 'antd';
+import { Card, Row, Col, Button, Icon, Spin } from 'antd';
 import { isEmpty } from 'lodash';
 import { getHeaderResourceName } from '../../../../../utils';
 import { formatCurrency } from '../../../../../utils/masks/formatCurrency';
@@ -15,6 +15,7 @@ import {
 
 import PrivatePageHeader from '../../../../components/PrivatePageHeader';
 import PrivatePageSection from '../../../../components/PrivatePageSection';
+import PrivatePageHeaderButton from '../../../../components/PrivatePageHeaderButton';
 import ProductsList from './components/ProductsList';
 
 import { Animated } from 'react-animated-css';
@@ -22,6 +23,8 @@ import { Animated } from 'react-animated-css';
 import './style.less';
 
 let i = 0;
+
+const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
 
 class OrderDetailsPage extends Component {
   constructor(props) {
@@ -49,8 +52,9 @@ class OrderDetailsPage extends Component {
       actions: { findOrder },
     } = this.props;
     findOrder(id).then((response) => {
+      const { payload } = response;
       this.setState({
-        order: response.payload,
+        order: payload,
       });
     });
   }
@@ -118,123 +122,153 @@ class OrderDetailsPage extends Component {
     return orders.results[i];
   }
 
+  renderOrderNumberStatus = (orderNumber, status) => (
+    <div>
+      <Row type="flex">
+        <div className="order-number">
+          #{orderNumber} <span>({status})</span>
+        </div>
+      </Row>
+      <Row className="order-actions" type="flex">
+        <PrivatePageHeaderButton>Faturar</PrivatePageHeaderButton>
+        <PrivatePageHeaderButton>Cancelar</PrivatePageHeaderButton>
+      </Row>
+    </div>
+  );
+
   render() {
-    let {
+    const {
       slide,
-      order: { customer, delivery, payment, orderItems },
+      order: { customer, delivery, payment, orderItems, orderNumber, status },
     } = this.state;
     console.log(this.state);
     return (
       <Fragment>
         <PrivatePageHeader
           title="Detalhes do Pedido"
+          content={this.renderOrderNumberStatus(orderNumber, status)}
           resourceMap={this.renderResourceMap()}
         />
-        <Animated animationIn="zoomIn" animationOut="zoomOut" isVisible={slide}>
-          <PrivatePageSection className="content-client-data">
-            <Row type="flex" gutter={24} justify="space-around">
-              <Col xs={24} sm={24} md={24} lg={24} xl={8}>
-                <Card className="card-order-data">
-                  <h3>Cliente</h3>
-                  <Row gutter={24} className="personal-info">
-                    <Col xs={24} sm={24} md={8} lg={8} xl={24}>
-                      <span className="label-info">Nome: </span>
-                      <span>{!isEmpty(customer) && customer.firstName}</span>
-                      <span> </span>
-                      <span>{!isEmpty(customer) && customer.lastName}</span>
-                    </Col>
-                    <Col xs={24} sm={24} md={8} lg={8} xl={24}>
-                      <span className="label-info">CPF: </span>
-                      <span>{!isEmpty(customer) && customer.cpf}</span>
-                    </Col>
-                    <Col xs={24} sm={24} md={8} lg={8} xl={24}>
-                      <span className="label-info">Telefone: </span>
-                      <span>{!isEmpty(customer) && customer.firstPhone}</span>
-                    </Col>
-                  </Row>
-                </Card>
-              </Col>
-              <Col xs={24} sm={24} md={24} lg={24} xl={8}>
-                <Card className="card-order-data">
-                  <h3>Endereço</h3>
-                  <Row type="flex" className="address-info" gutter={5}>
-                    <Col xs={24} sm={24} md={12} lg={12} xl={24}>
-                      <span className="label-info">Logradouro: </span>
-                      <span>
-                        {!isEmpty(delivery) && delivery.address.street}
-                      </span>
-                    </Col>
-                    <Col xs={24} sm={24} md={12} lg={12} xl={24}>
-                      <span className="label-info">Número: </span>
-                      <span>
-                        {!isEmpty(delivery) && delivery.address.number}
-                      </span>
-                    </Col>
-                    <Col xs={24} sm={24} md={12} lg={12} xl={24}>
-                      <span className="label-info">Complemento: </span>
-                      <span>
-                        {!isEmpty(delivery) && delivery.address.complement}
-                      </span>
-                    </Col>
-                    <Col xs={24} sm={24} md={12} lg={12} xl={24}>
-                      <span className="label-info">Cidade: </span>
-                      <span>{!isEmpty(delivery) && delivery.address.city}</span>
-                    </Col>
-                    <Col span={24}>
-                      <span className="label-info">Estado: </span>
-                      <span>
-                        {!isEmpty(delivery) && delivery.address.state}
-                      </span>
-                    </Col>
-                  </Row>
-                </Card>
-              </Col>
-              <Col xs={24} sm={24} md={24} lg={24} xl={8}>
-                <article className="payment-data">
-                  <h3>Pagamento</h3>
-                  <p className="payment-status">Aprovado</p>
-                  <Row className="payment-method">
-                    <span>Método de Pagamento</span>
-                    <p>
-                      Cartão de
-                      <span> </span>
-                      {!isEmpty(payment) && payment.paymentType}
-                    </p>
-                  </Row>
-                  <Row className="prices">
-                    <Col>
-                      <span className="freight-label">Frete:</span>
-                      <span>
-                        {' '}
+
+        <Row type="flex" justify="center">
+          {isEmpty(orderItems) ? (
+            <Spin indicator={antIcon} tip="Carregando" />
+          ) : null}
+        </Row>
+        {orderItems ? (
+          <Animated
+            animationIn="zoomIn"
+            animationOut="zoomOut"
+            isVisible={slide}
+          >
+            <PrivatePageSection className="content-client-data">
+              <Row type="flex" gutter={24} justify="space-around">
+                <Col xs={24} sm={24} md={24} lg={24} xl={8}>
+                  <Card className="card-order-data">
+                    <h3>Cliente</h3>
+                    <Row gutter={24} className="personal-info">
+                      <Col xs={24} sm={24} md={8} lg={8} xl={24}>
+                        <span className="label-info">Nome: </span>
+                        <span>{!isEmpty(customer) && customer.firstName}</span>
+                        <span> </span>
+                        <span>{!isEmpty(customer) && customer.lastName}</span>
+                      </Col>
+                      <Col xs={24} sm={24} md={8} lg={8} xl={24}>
+                        <span className="label-info">CPF: </span>
+                        <span>{!isEmpty(customer) && customer.cpf}</span>
+                      </Col>
+                      <Col xs={24} sm={24} md={8} lg={8} xl={24}>
+                        <span className="label-info">Telefone: </span>
+                        <span>{!isEmpty(customer) && customer.firstPhone}</span>
+                      </Col>
+                    </Row>
+                  </Card>
+                </Col>
+                <Col xs={24} sm={24} md={24} lg={24} xl={8}>
+                  <Card className="card-order-data">
+                    <h3>Endereço</h3>
+                    <Row type="flex" className="address-info" gutter={5}>
+                      <Col xs={24} sm={24} md={12} lg={12} xl={24}>
+                        <span className="label-info">Logradouro: </span>
+                        <span>
+                          {!isEmpty(delivery) && delivery.address.street}
+                        </span>
+                      </Col>
+                      <Col xs={24} sm={24} md={12} lg={12} xl={24}>
+                        <span className="label-info">Número: </span>
+                        <span>
+                          {!isEmpty(delivery) && delivery.address.number}
+                        </span>
+                      </Col>
+                      <Col xs={24} sm={24} md={12} lg={12} xl={24}>
+                        <span className="label-info">Complemento: </span>
+                        <span>
+                          {!isEmpty(delivery) && delivery.address.complement}
+                        </span>
+                      </Col>
+                      <Col xs={24} sm={24} md={12} lg={12} xl={24}>
+                        <span className="label-info">Cidade: </span>
+                        <span>
+                          {!isEmpty(delivery) && delivery.address.city}
+                        </span>
+                      </Col>
+                      <Col span={24}>
+                        <span className="label-info">Estado: </span>
+                        <span>
+                          {!isEmpty(delivery) && delivery.address.state}
+                        </span>
+                      </Col>
+                    </Row>
+                  </Card>
+                </Col>
+                <Col xs={24} sm={24} md={24} lg={24} xl={8}>
+                  <article className="payment-data">
+                    <h3>Pagamento</h3>
+                    <p className="payment-status">Aprovado</p>
+                    <Row className="payment-method">
+                      <span>Método de Pagamento</span>
+                      <p>
+                        Cartão de
+                        <span> </span>
+                        {!isEmpty(payment) && payment.paymentType}
+                      </p>
+                    </Row>
+                    <Row className="prices">
+                      <Col>
+                        <span className="freight-label">Frete:</span>
+                        <span>
+                          {' '}
+                          {!isEmpty(payment) &&
+                            formatCurrency(payment.paidValue)}
+                        </span>
+                      </Col>
+                      <span className="total-label">Valor total: </span>
+                      <span className="total-value">
                         {!isEmpty(payment) && formatCurrency(payment.paidValue)}
                       </span>
-                    </Col>
-                    <span className="total-label">Valor total: </span>
-                    <span className="total-value">
-                      {!isEmpty(payment) && formatCurrency(payment.paidValue)}
-                    </span>
-                  </Row>
-                </article>
-              </Col>
+                    </Row>
+                  </article>
+                </Col>
+              </Row>
+            </PrivatePageSection>
+            <Row
+              type="flex"
+              gutter={16}
+              justify="space-between"
+              className="antd-pro-components-setting-drawer-index-handle btn-paginate"
+              align="middle"
+              style={{ top: 400, width: '100%', marginLeft: 1 }}
+            >
+              <Button className="btn-prev" onClick={() => this.prevItem()}>
+                <Icon type="left" />
+              </Button>
+              <Button className="btn-next" onClick={() => this.nextItem()}>
+                <Icon type="right" />
+              </Button>
             </Row>
-          </PrivatePageSection>
-          <Row
-            type="flex"
-            gutter={16}
-            justify="space-between"
-            className="antd-pro-components-setting-drawer-index-handle btn-paginate"
-            align="middle"
-            style={{ top: 400, width: '100%', marginLeft: 1 }}
-          >
-            <Button className="btn-prev" onClick={() => this.prevItem()}>
-              <Icon type="left" />
-            </Button>
-            <Button className="btn-next" onClick={() => this.nextItem()}>
-              <Icon type="right" />
-            </Button>
-          </Row>
-          <ProductsList products={ orderItems } />
-        </Animated>
+            {orderItems ? <ProductsList products={orderItems} /> : null}
+          </Animated>
+        ) : null}
       </Fragment>
     );
   }
