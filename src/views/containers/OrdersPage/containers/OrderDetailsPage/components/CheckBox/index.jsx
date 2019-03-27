7ca6a65472d5fa3c43bb4b3ada19c356baa8fcff
Cauê Sampaio <caue.sampaio@it4solution.com.br>
2019-03-27 02:27:27 -0300

    Adding a CheckBox component to ProductsList, CheckAll done

---
+++ src/views/containers/OrdersPage/containers/OrderDetailsPage/components/CheckBox/index.jsx
@@ -0,0 +1,20 @@
+/*eslint-disable*/
+import React from 'react';
+
+export const CheckBox = (props) => {
+    console.log("PROPS", props)
+  return (
+    <li>
+      <input
+        key={props.channelSku.id}
+        onClick={props.handleCheckChieldElement}
+        type="checkbox"
+        checked={props.isChecked}
+        value={props.channelSku.refSku}
+      />{' '}
+      {props.value}
+    </li>
+  );
+};
+
+export default CheckBox;
--- src/views/containers/OrdersPage/containers/OrderDetailsPage/components/ProductsList/index.jsx
+++ src/views/containers/OrdersPage/containers/OrderDetailsPage/components/ProductsList/index.jsx
@@ -5,6 +5,7 @@ import { isEmpty } from 'lodash';
 
 import PrivatePageHeaderButton from '../../../../../../components/PrivatePageHeaderButton';
 import PrivatePageSection from '../../../../../../components/PrivatePageSection';
+import CheckBox from '../CheckBox';
 
 import './style.less';
 
@@ -12,19 +13,35 @@ class ProductList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: null,
+      products: [],
     };
   }
 
+  componentDidMount() {
+    const { products } = this.props;
+    this.setState({
+      products: products,
+    });
+  }
+
   renderTitle = (item) => (
-    <div style={{ display: 'flex', justifyContent: 'space-between',  whiteSpace: 'normal' }}>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        whiteSpace: 'normal',
+      }}
+    >
       {item.channelSku.description}
     </div>
   );
 
   renderAvatar = (item) => (
     <div style={{ display: 'flex' }}>
-      <Checkbox />
+      <CheckBox
+        handleCheckChieldElement={this.handleCheckChieldElement}
+        {...item}
+      />
       <img alt="" src={item.channelSku.images[0]} style={{ marginLeft: 8 }} />
     </div>
   );
@@ -37,16 +54,40 @@ class ProductList extends Component {
 
   renderDescription = (item) => (
     <div className="product-description">
-      <h3>Quantidade: <span>{item.quantity}</span></h3>
-      <h3>Preço: <span>{item.netValue}</span></h3>
-      <h3>SKU: <span>{item.channelSku.refSku}</span></h3>
-      <h3>Status: <span>{item.status}</span></h3>
+      <h3>
+        Quantidade: <span>{item.quantity}</span>
+      </h3>
+      <h3>
+        Preço: <span>{item.netValue}</span>
+      </h3>
+      <h3>
+        SKU: <span>{item.channelSku.refSku}</span>
+      </h3>
+      <h3>
+        Status: <span>{item.status}</span>
+      </h3>
     </div>
   );
 
+  handleAllChecked = (event) => {
+    let { products } = this.state;
+    products.forEach((product) => (product.isChecked = event.target.checked));
+    this.setState({ products: products });
+  };
+
+  handleCheckChieldElement = (event) => {
+    let { products } = this.state;
+    products.forEach((product) => {
+      if (product.channelSku.refSku == event.target.value)
+        product.isChecked = event.target.checked;
+    });
+    this.setState({ products: products });
+    console.log("target", event);
+  };
+
   render() {
-    const { products } = this.props;
-    console.log('render', products);
+    const { products } = this.state;
+    console.log('render', this.state);
     return (
       <div>
         <PrivatePageSection>
@@ -60,7 +101,9 @@ class ProductList extends Component {
           </Row>
           <Row>
             <Col className="space-bottom">
-              <Checkbox>Selecionar todos</Checkbox>
+              <Checkbox onClick={this.handleAllChecked} value="checkedall">
+                Selecionar todos
+              </Checkbox>
             </Col>
           </Row>
           <List
