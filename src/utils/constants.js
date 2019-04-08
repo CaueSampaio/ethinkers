95010b09c6bc1623f043c5dc0050196a14600581
Juliana Cerqueira de Abreu <juliana.cerqueira55@gmail.com>
2019-04-08 16:27:14 -0300

    url update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
-const constntApi =
+const constantApi =
   process.env.NODE_ENV === 'development'
-    ? 'https://private-ade86f-merhy.apiary-mock.com'
-    : `www.api.it4360.com.br`;
+    ? 'http://www.api.it4360.com.br'
+    : `http://www.api.it4360.com.br`;
 
-export const API = constntApi.replace(window.location.origin, '');
-
+export const API = constantApi.replace(window.location.origin, '');
+console.log(API);
 export const USER_DISCRIMINATORS = {
   company: 1,
   companyBranch: 2,
